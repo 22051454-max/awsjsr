c79010f0c8e55814acb1c1253ac32c41760768c8
22051454-max <22051454@kiit.ac.in>
2025-08-11 19:14:58 +0530

    Update website content

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Adibasi Welfare Society',
+  description: 'Created with opulencebyte',
+  generator: 'aws-jsr',
 }
 
 export default function RootLayout({
